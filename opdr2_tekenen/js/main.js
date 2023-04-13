49a13dab9f4578e4bee7dad650f5b405d1ba8dea
Arzoka <114647680+Arzoka@users.noreply.github.com>
2023-04-13 21:15:20 +0200

    <3

---
+++ opdr2_tekenen/js/main.js
@@ -0,0 +1,28 @@
+const cursor = document.getElementById("js--cursor");
+
+
+//spheres
+const sphere = document.getElementsByClassName("js--spheres");
+
+//boxes
+
+const boxes = document.getElementsByClassName("js--box");
+// laat de variabele i bij 0 beginnen, 
+for(let i = 0; i < boxes.length; i++){
+    boxes[i].onmouseenter = function(){
+        boxes[i].setAttribute("color",cursor.getAttribute("color"));
+
+    }
+
+}
+
+// laat de variabele i bij 0 beginnen, 
+
+for(let i = 0; i < sphere.length; i++){
+    sphere[i].setAttribute("color","#"+Math.floor(Math.random()*16777215).toString(16));
+    sphere[i].onmouseenter = function(){
+        cursor.setAttribute("color",sphere[i].getAttribute("color"));
+    }
+
+}
+
